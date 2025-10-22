fe44903caf3013a8d4e33ffdc26f0bb06475373a
Victor <victor.ciechovicz@merx.tech>
2025-10-22 08:43:58 -0300

    feat: implement docs swegger api-cursos

--- src/main/java/com/apicursos/ApiCursosApplication.java
+++ src/main/java/com/apicursos/ApiCursosApplication.java
@@ -1,10 +1,18 @@
 package com.apicursos;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Gestão de Cursos",
+                description = "API responsável pela gestão de cursos"
+        )
+)
 public class ApiCursosApplication {
 
     public static void main(String[] args) {
